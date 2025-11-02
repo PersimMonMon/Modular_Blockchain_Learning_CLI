22ebcc3453c4f5a59091275b3401f6e79f0f8971
PersimMonMon <buitho@oregonstate.edu>
2025-11-02 12:13:24 -0800

    display cost to users

--- blockchain_program/transactions.py
+++ blockchain_program/transactions.py
@@ -12,6 +12,11 @@ def add_transaction():
   sender = input("From     : ")
   receiver = input("To       : ")
   amount = input("Amount   : $")
+  print("")
+  print("Please review your data carefully before submitting.")
+  print("Enter 'yes' to confirm and submit your transaction to the blockchain, or 'no' to cancel it.")
+  print("Any other response will also cancel the transaction from being added to the blockchain.")
+  print("You can edit your transaction later if needed, but doing so may take additional time.\n")
   confirmation = input("Confirm? (yes/no) ")
 
   if confirmation == "yes":
